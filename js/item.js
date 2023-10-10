864aced7b78a8571bfbf206268cac4117aa0b12f
Walter ibarrola <walter_ibarrola@hotmail.com.ar>
2023-10-09 23:25:24 -0300

    agregando condicion para evitar negativos

--- js/item.js
+++ js/item.js
@@ -1,8 +1,12 @@
 const add=document.querySelector("#add");
 const subtract=document.querySelector("#subtract");
-const quantity=document.querySelector("#esteban");
+const quantity=document.querySelector("#campo_numerico");
 
 add.addEventListener("click",()=>quantity.value=Number(quantity.value)+1);
-subtract.addEventListener("click",()=>quantity.value=Number(quantity.value)-1);
-   
+
+subtract.addEventListener("click",() => {
+    if (Number(quantity.value) > 0) {
+(quantity.value=Number(quantity.value)-1);   
+    }
+});
 
