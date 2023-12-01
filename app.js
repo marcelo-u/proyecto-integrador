a66b1cfb2d3432232b9f2770c6ca2c75ffe3ca0b
Walteriba <walter_ibarrola@hotmail.com.ar>
2023-12-01 13:54:14 -0300

    instalacion modulos dotenv y mysql2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
 
 // Importacion de routes mainRoute
 const mainRoutes = require("./src/routes/mainRoutes");
@@ -26,4 +27,4 @@ app.use((req, res) => (
 
 app.get("/ping", (req,res) => res.send("pong"));
 
-app.listen(4000,() => console.log("Servidor de BlueLabel funcionando en http://localhost:4000"));
+app.listen(process.env.PORT ,() => console.log("Servidor de BlueLabel funcionando en http://localhost:4000"));
