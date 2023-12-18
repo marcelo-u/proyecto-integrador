e41cbcf415b33f367410b657e0108e4bf19d0f93
Walter ibarrola <walter_ibarrola@hotmail.com.ar>
2023-12-18 01:24:03 -0300

    toques finales, eliminar item tambien elimina la imagen

--- src/services/itemServices.js
+++ src/services/itemServices.js
@@ -1,4 +1,7 @@
-const {getAll, getFiltered, getAllAtributesFiltered, add, edit, deleteOne} = require("../models/itemsModels")
+const path = require('path');
+const fs = require('fs').promises
+const {getAll, getOne, getFiltered, getAllAtributesFiltered, add, edit, deleteOne} = require("../models/itemsModels");
+const { log } = require('console');
 
 const getNews = async () => {
     const items = await getAll();
@@ -28,7 +31,8 @@ const getRelated = async (object) => {
 
 const getItemsFromCategory = async (filter) => {
     let items = [];
-    min_filter = filter.toLowerCase()
+    // Asigna un valor por defecto
+    min_filter = (filter || "all").toLowerCase();
 
     switch (min_filter) {
     case "all":
@@ -68,23 +72,42 @@ const addItem = async (item, files) => {
 
 
 const editItem = async (item, files, id) => {
+    // Obtiene el item existente para conservar los datos actulales en las imagenes
+    const existingItem = await getOne(id);
+  
     const itemSchema = {
-      product_name: item.name,
-      product_description: item.description,
+      product_name: item.name, 
+      product_description: item.description ,
       price: item.price,
       stock: item.stock,
       discount: item.discount,
       sku: item.sku,
       dues: item.dues,
-      image_front: '/'+files[0].filename,
-      image_back: '/'+files[1].filename,
+      image_front: files && files.length > 0 && files[0].filename ? '/' + files[0].filename : existingItem.image_front,
+      image_back: files && files.length > 1 && files[1].filename ? '/' + files[1].filename : existingItem.image_back,
       licence_id: item.licence,
-      category_id: item.category
-    }
+      category_id: item.category,
+    };
+  
     return await edit(itemSchema, id);
-}
+  };
 
 const deleteItem = async (id) => {
+    const item = await getOne(id);
+    const path = "./public/images"
+    const files = [
+        (path + item.image_front), 
+        (path + item.image_back),
+    ];
+
+    Promise.all(files.map(file => fs.unlink(file)))
+    .then(() => {
+        console.log('Archivos removidos')
+    })
+    .catch(err => {
+        console.error('Algo salio mal', err)
+    })
+
     return await deleteOne(id);
 }
 
